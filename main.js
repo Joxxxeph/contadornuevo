4c2e274588010d0033a5b56fd5d951139a8fcff7
Joxxxeph <josephvasquezunefa@gmail.com>
2023-09-04 14:01:44 -0400

    actua

--- main.js
+++ main.js
@@ -6,26 +6,26 @@ let count = 0
 hh.textContent = count
 
 incre.addEventListener("click", function(){
-    count++
-    hh.textContent = count
-    if(count >= 5) hh.style.color = "#00bfff"
-    if(count >= 10) hh.style.color = "#e60023"
+    count++;
+    hh.textContent = count;
+    if(count >= 5) hh.style.color = "#00bfff";
+    if(count >= 10) hh.style.color = "#e60023";
         
     
-})
+});
 decre.addEventListener("click", function(){
     if (count > 0){
-        count --
-        hh.textContent= count
+        count --;
+        hh.textContent= count;
     }
-    if (count >= 0) hh.style.color = '#000'
-    if(count >= 5) hh.style.color = "#00bfff"
-    if(count >= 10) hh.style.color = "#e60023"
+    if (count >= 0) hh.style.color = '#000';
+    if(count >= 5) hh.style.color = "#00bfff";
+    if(count >= 10) hh.style.color = "#e60023";
 
-})
+});
 reset.addEventListener("click", function(){
-    count = 0
-    hh.textContent = count
-    hh.style.color = "#000"
-})
+    count = 0;
+    hh.textContent = count;
+    hh.style.color = "#000";
+});
 
