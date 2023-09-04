94af05ca5ac9e9b4ddb330723f273c319917d7f5
Joxxxeph <josephvasquezunefa@gmail.com>
2023-09-04 12:05:46 -0400

    nuevo contador

---
+++ main.js
@@ -0,0 +1,34 @@
+const hh = document.querySelector(".hh")
+const incre = document.querySelector(".incre")
+const decre = document.querySelector(".decre")
+const reset = document.querySelector(".reset")
+let count = 0
+hh.textContent = count
+
+incre.addEventListener("click", function(){
+    count++
+    hh.textContent = count
+    console.log(count)
+    if(count >= 5) hh.style.color = "#00bfff"
+    if(count >= 10) hh.style.color = "#e60023"
+        
+    
+})
+decre.addEventListener("click", function(){
+    if (count > 0){
+        count --
+        hh.textContent= count
+    }
+    if (count >= 0) hh.style.color = '#000'
+    if(count >= 5) hh.style.color = "#00bfff"
+    if(count >= 10) hh.style.color = "#e60023"
+
+})
+reset.addEventListener("click", function(){
+    count = 0
+    hh.textContent = count
+    hh.style.color = "#000"
+})
+
+
+console.log(count)
